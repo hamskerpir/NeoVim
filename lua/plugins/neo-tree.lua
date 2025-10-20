eba9632d3edc0aa7cb7ef8d4e1ef8dde550bc18c
pomo <re.markofdark@gmail.com>
2025-10-20 22:08:15 +0200

    fix: close neo-tree on file open

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,6 +10,14 @@ return {
     local ok, neotree = pcall(require, "neo-tree")
     if ok then
       neotree.setup({
+         event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+        },
         close_if_last_window = true,
         window = {
           mappings = {
