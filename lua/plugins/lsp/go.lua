ebb180d5a8aab8da7bdeb2038e8f0c6c86d73dd8
pomo <re.markofdark@gmail.com>
2025-12-03 16:49:03 +0100

    feat(lsp): add go

---
+++ lua/plugins/lsp/go.lua
@@ -0,0 +1,11 @@
+local lsp = "gopls"
+
+local lsp_helpers = require("plugins.lsp.helpers")
+
+vim.lsp.config(lsp, {
+  on_attach = lsp_helpers.get_on_attach(),
+  root_dir = vim.fs.root(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p"), { "go.mod", ".git" }),
+})
+
+return { lsp }
+
