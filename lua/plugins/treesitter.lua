2b741d2c6a38b0dd0e8c1a8e2d53f713f9cc209f
Mark Barzali <mark.barzali@solarwinds.com>
2026-06-23 11:34:38 +0200

    fix: logs + previous

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,38 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
+    lazy = false, -- Treesitter should ideally not be lazy-loaded
     opts = {
       ensure_installed = { "lua", "javascript", "python", "cpp", "terraform", "hcl", "vue" },
       highlight = { enable = true },
+      sync_install = false,
+      auto_install = true,
       indent = { enable = true },
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-
-      local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-
-      parser_configs.hcl = {
-        filetype = {"hcl", "terraform"}
+      local parser_config = require("nvim-treesitter.parsers")
+      parser_config.log = {
+        install_info = {
+          url = "https://github.com/Tudyx/tree-sitter-log",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
       }
+
+      table.insert(opts.ensure_installed, "log")
+      require("nvim-treesitter").setup(opts)
+
+    -- 2. DO NOT overwrite parser_configs. Use native filetype mapping instead.
+    -- This maps both .hcl and .tf files correctly without breaking runtime queries.
+    vim.filetype.add({
+      extension = {
+        hcl = "hcl",
+        tf = "terraform",
+        log = "log",
+      },
+    })
     end,
   },
 }
